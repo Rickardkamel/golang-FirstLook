26703dbfcf65fedbaa17b70e2877703dff2d34ab
rickardkamel <rickardkamel@gmail.com>
2017-09-08 17:14:20 +0200

    Struct pointers

--- 14_functions/15_passing-by-value/07_struct-pointer/main.go
+++ 14_functions/15_passing-by-value/07_struct-pointer/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
+type customer struct {
+	name string
+	age  int
+}
+
 func main() {
+	c1 := customer{"Rickard", 25}
+	fmt.Println(&c1.name) // memory address
+
+	changeMe(&c1)
+
+	fmt.Println(c1)       // {Rickard 25}
+	fmt.Println(&c1.name) // memory address
+}
+
+func changeMe(z *customer) {
+	fmt.Println(z)       // &{Rickard  25}
+	fmt.Println(&z.name) // memory address
+	z.name = "Ricky"
+	fmt.Println(z)       // &{Ricky 25}
+	fmt.Println(&z.name) // memory address
 
 }
