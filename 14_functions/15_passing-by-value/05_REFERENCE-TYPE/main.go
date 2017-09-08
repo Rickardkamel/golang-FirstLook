f52cd854a6ae8cd56e7a03fef365e7805265deef
rickardkamel <rickardkamel@gmail.com>
2017-09-08 08:35:37 +0200

    Reference types - make & map

--- 14_functions/15_passing-by-value/05_REFERENCE-TYPE/main.go
+++ 14_functions/15_passing-by-value/05_REFERENCE-TYPE/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	m := make(map[string]int)
+	changeMe(m)
+	fmt.Println(m["Todd"]) // 44
+}
+
+func changeMe(z map[string]int) {
+	z["Todd"] = 44
 }
--- 14_functions/15_passing-by-value/06_REFERENCE-TYPE/main.go
+++ 14_functions/15_passing-by-value/06_REFERENCE-TYPE/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	m := make([]string, 1, 25)
+	fmt.Println(m) // [ ]
+	changeMe(m)
+	fmt.Println(m) // [Rickard]
+}
+
+func changeMe(z []string) {
+	z[0] = "Rickard"
+	fmt.Println(z) // Rickard
 }
