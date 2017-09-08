3818270fcfc871e6988981b741aa44a3d9f1f019
rickardkamel <rickardkamel@gmail.com>
2017-09-08 07:58:30 +0200

    defer (usage & meaning of it).

--- 14_functions/14_defer/01_no-defer/main.go
+++ 14_functions/14_defer/01_no-defer/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+ world()
+ hello()
+}
+
+func hello() {
+	fmt.Print("hello ")
+}
+
+func world() {
+	fmt.Println("world")
 }
--- 14_functions/14_defer/02_with-defer/main.go
+++ 14_functions/14_defer/02_with-defer/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	defer world() // defers to right before main exits
+	hello()
+}
+
+func hello() {
+	fmt.Print("hello ")
+}
+
+func world() {
+	fmt.Println("world")
 }
