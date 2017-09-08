49b97076e610499d7c4b566d8bd9152a4a950f60
rickardkamel <rickardkamel@gmail.com>
2017-09-08 07:02:16 +0200

    variadic functions & slices.

--- 14_functions/08_variadic-args/main.go
+++ 14_functions/08_variadic-args/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	data := []float64{43, 56, 87, 12, 45, 57}
+	n := average(data...) // variadic argument
+	fmt.Println(n)
+}
+
+
+func average(sf ...float64) float64 { // variadic params
+	fmt.Println(sf)
+	fmt.Printf("%T \n", sf)
+	var total float64
+	for _, v := range sf { // for (index), (value) := range (variable)
+		total += v
+	}
+
+	return total / float64((len(sf)))
 }
--- 14_functions/09_slice-param-arg/main.go
+++ 14_functions/09_slice-param-arg/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	data := []float64{43, 56, 87, 12, 45, 57}
+	n := average(data)
+	fmt.Println(n)
+}
+
+
+func average(sf []float64) float64 {  // slice params
+	fmt.Println(sf)
+	fmt.Printf("%T \n", sf)
+	var total float64
+	for _, v := range sf { // for (index), (value) := range (variable)
+		total += v
+	}
+
+	return total / float64((len(sf)))
 }
