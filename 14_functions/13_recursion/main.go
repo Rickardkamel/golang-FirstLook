e2d2fe3eb0ee836a9e61ddf380f54939fd53e0db
rickardkamel <rickardkamel@gmail.com>
2017-09-08 07:58:08 +0200

    recursion and functions calling themselves.

--- 14_functions/13_recursion/main.go
+++ 14_functions/13_recursion/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(factorial(4))
+}
+
+func factorial(x int) int {
+	if x == 0 {
+		return 1
+	}
+
+	return x * factorial(x-1)
 }
